927ad6e521dd70013c0901dca72a636820ecc080
Nando Vieira <me@fnando.com>
2022-08-06 19:56:46 -0700

    Expand paths before setting to ENV.

--- SublimeEnv.py
+++ SublimeEnv.py
@@ -1,72 +1,77 @@
 import sublime, sublime_plugin
-from os import environ
+import os
 import subprocess
 import re
 
-settings = {}
-default_path = "/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/opt/X11/bin"
+default_path = "/opt/homebrew/bin:/opt/homebrew/sbin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/sbin:/opt/X11/bin"
+
+
+def settings(key):
+    return sublime.load_settings("SublimeEnv.sublime-settings").get(key)
+
 
 def debug(*args):
-  global settings
+    if settings("debug"):
+        print("[SublimeEnv]", *args)
 
-  if settings.get("debug"):
-    print("[SublimeEnv]", *args)
 
 def run_command(command):
-  result = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
-  output = result.stdout.read().decode("utf-8").rstrip()
+    result = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    output = result.stdout.read().decode("utf-8").rstrip()
 
-  return output
+    return output
+
 
 def retrieve_path(command):
-  if not command:
-    return
+    if not command:
+        return
 
-  path = run_command(command.format(user=environ["USER"], shell=environ["SHELL"]))
+    path = run_command(
+        command.format(user=os.environ["USER"], shell=os.environ["SHELL"]))
 
-  system_dirs = []
-  user_dirs = []
-  dirs = path.split(":")
+    system_dirs = []
+    user_dirs = []
+    dirs = path.split(":")
 
-  for dir in dirs:
-    if re.match(r"^/(usr|bin|sbin|opt)", dir):
-      system_dirs.append(dir)
-    else:
-      user_dirs.append(dir)
+    for dir in dirs:
+        if re.match(r"^/(usr|bin|sbin|opt)", dir):
+            system_dirs.append(dir)
+        else:
+            user_dirs.append(dir)
 
-  path = ":".join(user_dirs + system_dirs)
+    path = ":".join(user_dirs + system_dirs)
 
-  return path
+    return path
+
 
 def plugin_loaded():
-  global settings
+    debug("Starting")
+    platform = sublime.platform()
+    env = settings("env")
+    commands = settings("commands")
 
-  settings = sublime.load_settings("SublimeEnv.sublime-settings")
+    debug("Setting environment for", platform)
 
-  debug("Starting")
-  platform = sublime.platform()
-  env = settings.get("env")
-  commands = settings.get("commands")
-
-  debug("Setting environment for", platform)
-
-  if not "PATH" in env:
-    debug("PATH is not set, so infer from system")
-    path = retrieve_path(commands[platform])
-
-    if path:
-      debug("Inferred PATH is", path)
-      env["PATH"] = path
+    if "PATH" in env:
+        path = env["PATH"]
     else:
-      debug("Couldn't infer PATH, so defaulting to", default_path)
-      env["PATH"] = default_path
+        debug("PATH is not set, so infer from system")
+        path = retrieve_path(commands[platform])
 
-  for name, value in env.items():
-    debug("Setting %s=%s" % (name, value))
-    environ[name] = value
+        if not path:
+            debug("Couldn't infer PATH, so defaulting to", default_path)
+            path = default_path
 
-  debug("Finished")
+    env["PATH"] = ":".join(
+        [os.path.expanduser(item) for item in path.split(":")])
+
+    for name, value in env.items():
+        debug("Setting %s=%s" % (name, value))
+        os.environ[name] = value
+
+    debug("Finished")
+
 
 class ReloadEnvCommand(sublime_plugin.TextCommand):
-  def run(self, edit):
-    plugin_loaded()
+    def run(self, edit):
+        plugin_loaded()
